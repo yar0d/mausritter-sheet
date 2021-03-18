033ba579b2f7c02c12afb531173c28815618a7ca
Daniel Coquette <dcoquette@scalair.fr>
2021-03-18 14:39:17 +0100

    thore: Add version in about dialog

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,5 +1,8 @@
 <template>
   <w-dialog v-model="showDialog" :width="400" transition="scale" :title="$t('About')" content-class="w-flex column align-start justify-center pa2">
+    <div>
+      Mausritter Sheet {{ version }}
+    </div>
     <div class="mt4">
       {{ $t('This application is an independent production by Daniel Coquette and is not affiliated with Losing Games. It is published under the Mausritter Third Party Licence.') }}
     </div>
@@ -7,7 +10,6 @@
     <div class="mt4">{{ $t('This application is copyright Daniel Coquette.') }}</div>
     <div class="mt4">{{ $t('Sheet, bank and drawer backgrounds from DevianArt under license') }} <a href="https://creativecommons.org/licenses/by/3.0/">CC Attribution 3.0 License</a> and <a href="https://creativecommons.org/licenses/by-nc-nd/3.0/">CC Attribution-Noncommercial-No Derivative Works 3.0 License</a></div>
     <div class="mt4">{{ $t('Welcome, History and Hirelings background from') }} <a href="https://www.deviantart.com/irbeus">irbeus</a> {{ $t('under licence') }} <a href="https://creativecommons.org/licenses/by/3.0/">CC Attribution 3.0 License</a> and <a href="https://creativecommons.org/licenses/by-nc-nd/3.0/">CC Attribution-Noncommercial-No Derivative Works 3.0 License</a></div>
-    <!-- <div class="mt4">{{ $t('Hirelings background from') }} <a href="https://www.deviantart.com/clintcearley">clintcearley</a> {{ $t('under licence') }}</div> -->
     <!-- <div class="mt4">{{ $t('Additionnals rules from ') }} <a href="https://sites.google.com/view/talesfrommoonshore/">Tales from Moonshore</a>.</div> -->
     <div class="w-max text-right">
       <w-button class="mt6" @click="showDialog = false" bg-color="error" dark>{{ $t('Close') }}</w-button>
@@ -16,11 +18,14 @@
 </template>
 
 <script>
+import { version } from '../../package.json'
+
 export default {
   name: 'About',
   data () {
     return {
-      showDialog: false
+      showDialog: false,
+      version
     }
   },
   methods: {
