49e9c5e2a49b9f298f240da0d61ba9b73df78755
yar0d <daniel.coquette@gmail.com>
2021-04-15 14:33:50 +0200

    fix: Translation switcher

--- src/main.js
+++ src/main.js
@@ -11,9 +11,7 @@ import '../public/libs/dice3d/dice.css'
 
 import { store } from './store'
 
-let i18nModule
-if (process.env.NODE_ENV !== 'production') i18nModule = require('vue-i18n')
-else i18nModule = require('vue-i18n/dist/vue-i18n.runtime.esm-browser.prod')
+import { createI18n } from 'vue-i18n'
 
 import en_US from './locales/en-US.json'
 import mouse_en_US from './locales/mouse.en-US.json'
@@ -21,17 +19,20 @@ import items_en_US from './locales/items.en-US.json'
 import conditions_en_US from './locales/conditions.en-US.json'
 import spells_en_US from './locales/spells.en-US.json'
 import hirelings_en_US from './locales/hirelings.en-US.json'
+
 import fr_FR from './locales/fr-FR.json'
 import mouse_fr_FR from './locales/mouse.fr-FR.json'
 import items_fr_FR from './locales/items.fr-FR.json'
 import conditions_fr_FR from './locales/conditions.fr-FR.json'
 import spells_fr_FR from './locales/spells.fr-FR.json'
 import hirelings_fr_FR from './locales/hirelings.fr-FR.json'
+
 import { datetimeFormats } from './services/locales.js'
 
-
-const i18n = i18nModule.createI18n({
+// Set locales
+export const i18n = createI18n({
   locale: 'en-US',
+  fallbackLocale: 'en-US',
   messages: {
     'en-US': { ...en_US, ...mouse_en_US, ...items_en_US, ...conditions_en_US, ...hirelings_en_US, ...spells_en_US },
     'fr-FR': { ...fr_FR, ...mouse_fr_FR, ...items_fr_FR, ...conditions_fr_FR, ...hirelings_fr_FR, ...spells_fr_FR }
