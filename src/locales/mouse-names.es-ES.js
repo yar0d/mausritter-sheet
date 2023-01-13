466169ebe072533240f514fdc1f3654f8d6a364d
Daniel Coquette <daniel.coquette@gmail.com>
2023-01-13 12:13:12 +0100

    I18n/language/spanish (#41)
    * feat: Add Español
    chore: upgrade to node 14
    chore: upgrade to Tauri 1.1
    
    * chore(release): 1.11.0
    
    Co-authored-by: Daniel Coquette <dcoquette@scalair.fr>

---
+++ src/locales/mouse-names.es-ES.js
@@ -0,0 +1,8 @@
+export const matrinames = [ 'Alcantarilla', 'Bigotetieso', 'Cobaya', 'Colalarga', 'Correrriesgo', 'Dientetorcido', 'Espino', 'Estival', 'Hacedor', 'Invernal', 'Lenguaraz', 'Liron', 'Mantecona', 'Montaciervos', 'Parmesano', 'Pepita', 'Rascatunel', 'Saltagatos', 'Tirasemillas', 'Turrolate' ]
+
+export const names = [ 'Ada', 'Abril', 'Abundio', 'Acebo', 'Agata', 'Ajenjo', 'Aliso', 'Amapola', 'Ambrosio', 'Anís', 'Atilano', 'Augusto', 'Avellana', 'Azafrán', 'Basil', 'Belladona', 'Benigna', 'Boletus', 'Boniato', 'Brandi', 'Brezo', 'Brie', 'Caléndula', 'Camembert', 'Cardo', 'Casilda', 'Cayena', 'Cebollino', 'Centola', 'Cereza', 'Cheddar', 'Clarisa', 'Coco', 'Colin', 'Colombo', 'Comino', 'Enebro', 'Endrina', 'Ero', 'Esmeralda', 'Estragón', 'Fermín', 'Feta', 'Flora', 'Francis', 'Gallipierno', 'Gil', 'Ginkgo', 'Gracia', 'Gouda', 'Gruyer', 'Hipérico', 'Horacio', 'Iris', 'Jacinto', 'Jazmín', 'Jerónima', 'Laurel', 'Lavanda', 'Lentisco', 'Limoncillo', 'Lirio', 'Lorenzo', 'Macadamia', 'Magnolia', 'Mahón', 'Margarita', 'Martín', 'Mate', 'Mejorana', 'Mirto', 'Miso', 'Nori', 'Oliva', 'Perla', 'Picón', 'Ricino', 'Román', 'Romanescu', 'Roque', 'Rufina', 'Sauce', 'Sándalo', 'Saturnino', 'Simón', 'Shitake', 'Stilton', 'Tofu', 'Valentina', 'Valero', 'Violeta', 'Yuca', 'Zoilo' ]
+
+export default {
+  matrinames,
+  names
+}
